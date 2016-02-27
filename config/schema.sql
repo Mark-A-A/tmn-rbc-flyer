de78e21847c93ce0fd74c4b52498861ac7071d06
Mark-A-A <markawai01@gmail.com>
2016-02-27 13:16:18 -0500

    started schema for user registrationa nd login. added pictures

--- config/schema.sql
+++ config/schema.sql
@@ -1,2 +1,35 @@
 CREATE DATABASE virtual_flyer_db;
 USE virtual_flyer_db;
+
+
+CREATE TABLE users (
+id int NOT NULL AUTO_INCREMENT,
+first_name varchar(255) NOT NULL,
+last_name varchar(255) NOT NULL,
+email varchar(255) NOT NULL,
+username varchar(255) NOT NULL,
+password varchar(255) NOT NULL,
+location varchar(255) NOT NULL,
+student boolean DEFAULT 0 NOT NULL,
+teacher boolean DEFAULT 0 NOT NULL,
+createdDate timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+lastModified timestamp NOT NULL DEFAULT now() on update now()
+authenticated boolean DEFAULT 0 NOT NULL
+
+);
+
+CREATE TABLE posts (
+id int NOT NULL AUTO_INCREMENT,
+first_name varchar(255) NOT NULL,
+last_name varchar(255) NOT NULL,
+username varchar(255) NOT NULL,
+email varchar(255) NOT NULL,
+password varchar(255) NOT NULL,
+location varchar(255) NOT NULL,
+student boolean DEFAULT 0 NOT NULL,
+teacher boolean DEFAULT 0 NOT NULL,
+createdDate timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+lastModified timestamp NOT NULL DEFAULT now() on update now()
+authenticated boolean DEFAULT 0 NOT NULL
+
+);
