9ca2997d3f2de500c4db5a2b5a7a0b9346a98f64
Neelesh <ntekal@gmail.com>
2016-03-05 08:19:26 -0500

    modals populated with information from events database

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,2 +1,7 @@
-$('.datepicker').pickadate();
+$('.datepicker').pickadate({
+  format: 'You selecte!d: dddd, dd mmm, yyyy',
+  formatSubmit: 'yyyy/mm/dd',
+  hiddenPrefix: 'prefix__',
+  hiddenSuffix: '__suffix'
+});
 
